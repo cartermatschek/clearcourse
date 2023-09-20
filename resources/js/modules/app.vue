5496d08617962181b5f964a043c43e7ef36d53ae
Camron Matschek <camron@camroncade.com>
2023-09-20 15:35:18 -0500

    auth setup for SPA with Vue

--- resources/js/modules/app.vue
+++ resources/js/modules/app.vue
@@ -1,13 +1,20 @@
 <template>
-    {{ welcomeMessage }}
+    Welcome, {{ store.user.name }}!
 </template>
 
 <script>
+    import { store } from './../store.js'
+
     export default {
         data() {
             return {
-                welcomeMessage: 'Welcome To The Dashboard!',
+                store,
             }
         },
+        created() {
+            axios.get('/api/user').then((response) => {
+                store.user = response.data;
+            })
+        }
     }
 </script>
